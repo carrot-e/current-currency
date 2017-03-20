cd233b12fad081152176af3ac48fbd3d80849fce
elena <elenalistratova11@gmail.com>
2017-03-20 21:55:34 +0200

    Vue-powered front-end: initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="app">
+        <input v-model.number="userValue" v-on:keypress="isCurrency()" type="number" step="any">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                userValue: ''
+            }
+        },
+        methods: {
+            isCurrency: function (evt) {
+                evt = (evt) ? evt : window.event;
+                var charCode = (evt.which) ? evt.which : evt.keyCode;
+                if ((charCode > 31 && (charCode < 48 || charCode > 57)) && charCode !== 46) {
+                    evt.preventDefault();
+                } else {
+                    return true;
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    #app {
+        /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+
+    }
+
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+</style>
